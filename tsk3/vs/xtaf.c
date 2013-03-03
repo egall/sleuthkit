8ccbb1006c73523b2fff6afc1e2475b79461f689
Alex Nelson <ajnelson@cs.ucsc.edu>
2013-03-02 23:50:17 -0800

    Revert change for unexpected behaviors

--- tsk3/vs/xtaf.c
+++ tsk3/vs/xtaf.c
@@ -71,7 +71,7 @@ tsk_vs_xtaf_open(TSK_IMG_INFO * img_info, TSK_DADDR_T offset, uint8_t test)
 
     /* inititialize settings */
     vs->part_list = NULL;
-    vs->endian = TSK_BIG_ENDIAN;
+    vs->endian = 0x02; /*AJN TODO Setting this to TSK_BIG_ENDIAN, which is the same value, causes XTAF recognition to immediately fail...why?*/
     vs->block_size = 512;
 
     /* Assign functions */
